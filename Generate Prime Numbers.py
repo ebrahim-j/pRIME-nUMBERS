312ac5992884a0961f1eaf19ed3b51f77543bba2
Husain Janoowalla <Husain Janoowalla>
2017-02-06 17:07:09 +0300

    Funtions required for generating prime numbers 0 - n (I think!)

---
+++ Generate Prime Numbers.py
@@ -0,0 +1,22 @@
+import Math
+
+def isPrime(n):
+	if n == 1 :
+		return True
+	elif n % 2 == 0:
+		return False
+	else:
+		for i in range(3,n,2):
+			if n % i == 0:
+				return False
+			else:
+				return True
+
+
+
+def GeneratePrime(n):
+	n = int(input("Insert here: "))
+	for i in range(n+1):
+		if isPrime(i) == True:
+			print(i)
+
